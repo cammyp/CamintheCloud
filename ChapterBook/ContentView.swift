311570c7bf427b9dc941e6497c4ea2acb6fecd29
Cameron Partee <cp3082@nyu.edu>
2020-04-23 13:33:21 -0400

    Major user interface changes and new navigation layout

--- ChapterBook/ContentView.swift
+++ ChapterBook/ContentView.swift
@@ -3,16 +3,14 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        NavigationView {
-            TableOfContents()
-            .navigationBarTitle("")
-            .navigationBarHidden(true)
+        VStack {
+            PageTemplate()
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
