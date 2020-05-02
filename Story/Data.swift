5f7a898c078d145d176bc03ff2cebb7569fd71d9
Cameron Partee <cp3082@nyu.edu>
2020-05-01 22:37:46 -0400

    double checked spelling and grammer

--- Story/Data.swift
+++ Story/Data.swift
@@ -3,14 +3,14 @@ import Foundation
 struct StoryText {
     var text = [
         "",
-        "Cameron was at home when she got an angry call from her boss. “Cameron you said the app would be ready today! Where is the app?”  Cameron was confused, she had uploaded the app to the App Store last night.￼ “Let me troubleshoot and see what’s going on.” Cameron said.",
-        "She went to the App Store and downloaded the app. She opened the app and found that none of the content was loading. Since the app is available for download, but the data in the app was not loading Cameron knew something was wrong with the app's database connection.",
+        "Cameron was at home when he got an angry call from his boss. “Cameron you said the app would be ready today! Where is the app?”  Cameron was confused,he had uploaded the app to the App Store last night.￼ “Let me troubleshoot and see what’s going on.” Cameron said.",
+        "He went to the App Store and downloaded the app. He opened the app and found that none of the content in the app was loading. Since the app is available for download, but the data in the app is not loading Cameron knew something was wrong with the app's database connection.",
         "With all the data and files you have access to, there is no possible way to store that information on your physical device. The cloud stores and accesses data and programs over the Internet. When you think of the cloud you can imagine a gigantic server-farm infrastructure.",
         "Clouds are up high in the sky, but the cloud is just a metaphor for the Internet. Despite being held in the cloud, your data still needs to be physically stored on a device somewhere. Unfortunately, you will never truly know where your data is unless you do some digging.",
-        "Servers may be in China, the UK or anywhere else in the world. Cameron needed to figure out why the AWS data center was not allowing her data to be accessed. She needed to figure this out or her boss would be very upset with her!",
-        "After doing a couple hours of research Cameron found that one specific AWS data center in North Virginia was experiencing an outage. Not only did their power fail but their back up generators failed as well. This means that anyone trying to use the app has no access to its data",
-        "Cameron immediately called her boss to inform her of the situation. This is an extremely rare situation boss lady. The cloud is amazing tool for storing and accessing information and rarely has issues. It should only be down for a couple of hours.",
-        "About four hours later the app was being used by customers all around the world! The cloud was back up and running and providing people with all the information they desire!",
+        "Data servers could be in China, the UK or anywhere else in the world. Cameron needed to figure out why the Amazon Web Service data center was not allowing his data to be accessed inside the app. The sooner the fix, the sooner the app could start getting downloads and making money.",
+        "After a couple hours of research Cameron found that one specific Amazon Web Service data center in North Virginia was experiencing an outage. Not only did their power fail but their back up generators failed as well. Anyone currently using the app has no access to its data.",
+        "Cameron immediately called his boss to inform him of the situation. This is an extremely rare situation. You see, the cloud is an amazing tool for storing and accessing information and programs. The cloud rarely has issues due to its horizontal scalability. The outage should be resolved soon.",
+        "Around four hours later the Amazon Web Service data center had their power restored. The app was being used by hundreds of thousands of customers all around the world! The cloud was back up and running and providing people with all the information they needed and desired!",
         ""
     ]
 }
